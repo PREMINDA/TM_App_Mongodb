bfcf67bfddcefbaa775a1a8cf3c4316fa262aa96
PREMINDA <premindabandara0777@gmail.com>
2021-05-19 02:21:09 +0530

    end points for Get and Post User data

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const User = mongoose.model("User", {
+const User = mongoose.model("NewUser", {
   name: {
     type: String,
     required: true,
